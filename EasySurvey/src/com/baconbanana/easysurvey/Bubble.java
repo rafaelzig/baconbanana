dc00663db92184c8cdd26485ccdb015eb3df64c8
Rafael da Silva Costa <k1209810@kcl.ac.uk>
2014-02-11 18:23:01 +0000

    Removed unused imports

--- EasySurvey/src/com/baconbanana/easysurvey/Bubble.java
+++ EasySurvey/src/com/baconbanana/easysurvey/Bubble.java
@@ -4,7 +4,6 @@ import java.util.Random;
 
 import android.animation.ObjectAnimator;
 import android.content.Context;
-import android.graphics.Canvas;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
--- EasySurvey/src/com/baconbanana/easysurvey/MainActivity.java
+++ EasySurvey/src/com/baconbanana/easysurvey/MainActivity.java
@@ -1,6 +1,5 @@
 package com.baconbanana.easysurvey;
 
-import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
