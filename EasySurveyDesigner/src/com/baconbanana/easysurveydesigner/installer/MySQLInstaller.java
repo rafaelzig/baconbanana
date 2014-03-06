71a996d5105ebe3719ae8a2432dab161c7188e09
rollies <tommylewis121@hotmail.co.uk>
2014-03-06 19:54:32 +0000

    Database installer

---
+++ EasySurveyDesigner/src/com/baconbanana/easysurveydesigner/installer/MySQLInstaller.java
@@ -0,0 +1,84 @@
+package com.baconbanana.easysurveydesigner.installer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class MySQLInstaller {
+	public static void main(String[] args){
+		String workingDir = System.getProperty("user.dir");
+		File scrFolder = new File(workingDir + "\\Database");
+		//check that system is 64bit or not
+		File destFolder = null;
+		//get location of my documents
+		String docLoc = System.getenv("USERPROFILE") + "\\My Documents";
+		try{
+			destFolder = new File(docLoc);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		MySQLInstaller sqlLite = new MySQLInstaller();
+		sqlLite.copyFolder(scrFolder, destFolder);
+		sqlLite.installSQLLite(docLoc);
+	}
+	
+	public void copyFolder(File scr, File dest){
+		//Create file directories
+		
+		if(scr.isDirectory()){
+			if(!dest.exists()){
+				dest.mkdirs();
+			}
+			//get child dir/files names
+			String files[] = scr.list();
+			//iterrate though folder structure copying and creating
+			for(String file : files){
+				File scrFile = new File(scr, file);
+				File destFile = new File(dest, file);
+				copyFolder(scrFile, destFile);
+			}
+		}else{
+			//create files in directories
+			InputStream in = null;
+			OutputStream out = null;
+			//set up input and outputs to copy files
+			try{
+			in = new FileInputStream(scr);
+			out = new FileOutputStream(dest);
+			}catch(FileNotFoundException e){
+				e.printStackTrace();
+			}
+			//create buffer so that any type of file can be copied
+			byte[] buffer = new byte[1024];
+			int length;
+			//copy file across
+			try {
+				while((length = in.read(buffer)) > 0){
+					out.write(buffer, 0, length);
+				}
+				in.close();
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	public void installSQLLite(String loc){
+		//Get the command propt
+		Runtime rt = Runtime.getRuntime();
+		Process p = null;
+		String comand = loc + "\\SQLite\\sqlite3";
+		//Can also create database by apending it to the end of staement.
+		try{
+			System.out.println(comand);
+			p = rt.exec(comand);
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+}
