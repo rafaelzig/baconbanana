4d57c9ef184723a9bdf45e17393ce61b9a63838d
rollies <tommylewis121@hotmail.co.uk>
2014-03-18 11:50:33 +0000

    Make Enum class for question types

--- EasySurveyDesigner/src/com/baconbanana/easysurveydesigner/functionalCore/models/QuestionType.java
+++ EasySurveyDesigner/src/com/baconbanana/easysurveydesigner/functionalCore/models/QuestionType.java
@@ -9,5 +9,41 @@ package com.baconbanana.easysurveydesigner.functionalCore.models;
  */
 public enum QuestionType
 {
-	NUMERIC, DATE, TEXTUAL, MULTIPLE_CHOICE, MULTIPLE_ANSWER, SCALAR, CONTINGENCY;
+	NUMERIC("Numeric"), DATE("Date"), TEXTUAL("Textual"), MULTIPLE_CHOICE("MultiChoice"), MULTIPLE_ANSWER("MultiAnswer"), SCALAR("Scalar"), CONTINGENCY("Contingency");
+	
+	private String type;
+	//Constructor to assign database values
+	private QuestionType(String type){
+		this.type = type;
+	}
+	
+	@Override
+	public String toString(){
+		String result = "";
+		switch(this){
+		case NUMERIC :
+			result = "Numeric Question";
+			break;
+		case DATE :
+			result = "Date Question";
+			break;
+		case TEXTUAL : 
+			result = "Open Ended Text Question";
+			break;
+		case MULTIPLE_CHOICE :
+			result = "Multiple Choice Question";
+			break;
+		case MULTIPLE_ANSWER :
+			result = "Multiple Answer Question";
+			break;
+		case SCALAR :
+			result = "Rating Question";
+			break;
+		case CONTINGENCY :
+			result = "Contingency Question";
+			break;
+		}
+		return result;
+		
+	}
 }
