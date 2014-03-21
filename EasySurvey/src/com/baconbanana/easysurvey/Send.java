9a0f04def999dc22ce19d271313a6cedf5eb306b
bekasekadiskoteka <k1211220@kcl.ac.k>
2014-03-21 12:20:49 +0000

    INtegration of my:  one huge class connectionActivity divided into:  -connectionActivity  -ConnectToServer  -Get  -Send  -WaitForInput
     2) removed all not needed lines in Rafa's class survey activity and 
     added one line

---
+++ EasySurvey/src/com/baconbanana/easysurvey/Send.java
@@ -0,0 +1,71 @@
+package com.baconbanana.easysurvey;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.baconbanana.easysurvey.functionalCore.Storage;
+import com.baconbanana.easysurveydesigner.functionalCore.parsing.Operations;
+
+
+/**
+* This class tries to send saved answers from internal storage to output via socket 
+ * that it gets from constructor. After attempting, shows dialog box with message in
+ * ConnectionActivity
+ * @author Beka, Team BaconBanana
+ *
+ */
+public class Send extends AsyncTask<String, Void, String> {
+	String message = "";
+	Socket skt;
+	Context context;
+	
+	/**
+	 * @param s Socket
+	 * @param c Context
+	 */
+	public Send(Socket s, Context c){
+		this.skt=s;
+		this.context=c;
+	}
+	@Override
+	protected String doInBackground(String... arg0) {
+
+		try {
+			PrintStream output = null;
+			output = new PrintStream(skt.getOutputStream());
+
+			String s = Storage.readFromInternal(context,
+					Operations.FILENAME);
+			Log.d("storge read", s);
+			String encrypted = EncryptionJ.encryptMsg(s);
+
+			Log.d("storge read", encrypted);
+
+			output.print(encrypted);
+			message = "successfully sent";
+
+		} catch (IOException e) {
+			System.out.println(e);
+			message = "could not send it";
+		} 
+		return null;
+	}
+
+	protected void onPostExecute(String result) {
+
+		AlertDialog.Builder dlgAlert = new AlertDialog.Builder(context);
+
+		dlgAlert.setMessage(message);
+		dlgAlert.setPositiveButton("OK", null);
+		dlgAlert.create().show();
+		Log.d("postexecute", "connect toServer");
+
+	}
+
+}
