c2b03432a06c1a76c1afed5f3874db2dd34923e6
LazyFucker <Matthewkusaj@gmail.com>
2014-03-24 17:58:44 +0000

    Move button on Create survey works. Just needs require statement on that to avoid one of the errors

--- EasySurveyDesigner/src/com/baconbanana/easysurveydesigner/newGUI/CreateSurvey.java
+++ EasySurveyDesigner/src/com/baconbanana/easysurveydesigner/newGUI/CreateSurvey.java
@@ -153,8 +153,10 @@ public class CreateSurvey extends SQLWindow{
 			//TODO delete
 		}
 		else if(e.getSource().equals(moveBtn)){
+			if (!(templateList.getSelectedValue() == null)){
 			surveyPrevModel.insertElement("Survey_Template", DBController.appendApo(this.surveyName), DBController.appendApo(templateModel.getElementAt(templateList.getSelectedIndex())));
 			surveyPrevModel.getData("Survey_Template", "Survey = " + DBController.appendApo(this.surveyName), 1, "Survey", "Template");
+			}
 		}
 		else if(e.getSource().equals(saveBtn)){
 			//TODO save
