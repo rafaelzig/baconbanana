787c0d5196341dcf968031a719718d10d7bf3fa5
LazyFucker <Matthewkusaj@gmail.com>
2014-03-26 00:04:52 +0000

    gui for patient selector :)

--- EasySurveyDesigner/src/com/baconbanana/easysurveydesigner/newGUI/PatientSelector.java
+++ EasySurveyDesigner/src/com/baconbanana/easysurveydesigner/newGUI/PatientSelector.java
@@ -1,13 +1,16 @@
 package com.baconbanana.easysurveydesigner.newGUI;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
 
 import com.baconbanana.easysurveydesigner.functionalCore.dbops.DBController;
 import com.baconbanana.easysurveydesigner.functionalCore.models.SQLList;
@@ -39,23 +42,47 @@ public class PatientSelector extends Window{
 		
 		patientListModel = new SQLList("Patient", 0, "Patient");
 		patientList =new JList<String>(patientListModel);
-		patientSurveyListModel = new SQLList("Patient NATURAL JOIN Patient_Survey", "Patient =" + DBController.appendApo(patientList.getSelectedValue()), 0,"PatientID", "Patient", "Survey");
+		patientSurveyListModel = new SQLList("Patient NATURAL JOIN Patient_Survey", "N =" + DBController.appendApo(patientList.getSelectedValue()), 0,"PatientID", "Patient", "Survey");
 		patientSurveyList = new JList<String>(patientSurveyListModel);
-		patientListModel.getData();
+//		patientListModel.getData();
 		patientSelectionModel=patientList.getSelectionModel();
 		
 		getWindow().setLayout(new BorderLayout());
 		patientLbl = new JLabel("List of Patients");
 		getWindow().add(patientLbl, BorderLayout.NORTH);
-		JPanel centerPanel = new JPanel();
 		
+		JPanel centerPanel = new JPanel(new GridLayout(1, 2));
+		centerPanel.add(new JScrollPane(patientList));
+		patientList.setBorder(getBorder());
+		centerPanel.add(new JScrollPane(patientSurveyList));
+		patientSurveyList.setBorder(getBorder());
+		getWindow().add(centerPanel, BorderLayout.CENTER);
 		
+		JPanel southPanel = new JPanel(new GridLayout(1,2));
+		selectBtn = new JButton("Select");
+		southPanel.add(selectBtn);
+		cancelBtn = new JButton("Cancel");
+		southPanel.add(cancelBtn);
+		getWindow().add(southPanel, BorderLayout.SOUTH);
+		
+//		patientSelectionModel.addListSelectionListener(this);
+		selectBtn.addActionListener(this);
+		cancelBtn.addActionListener(this);
+		
+		setFrameOptions();
+		// TODO FIX PACK!!!!!!!
+		getWindow().pack();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
+	}
+	public void valueChanged(ListSelectionEvent e) {
+		if(e.getSource().equals(patientSelectionModel) && patientSelectionModel.getValueIsAdjusting() == false){
+			
+		}
 	}
 
 }
