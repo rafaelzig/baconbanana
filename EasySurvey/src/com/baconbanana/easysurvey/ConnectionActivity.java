7d5982a5afa260cd58795c721415e119d7364574
Rafael da Silva Costa <rafael.silva_costa@kcl.ac.uk>
2014-03-26 05:05:47 +0000

    Modified GUI of android connection activity a bit

--- EasySurvey/src/com/baconbanana/easysurvey/ConnectionActivity.java
+++ EasySurvey/src/com/baconbanana/easysurvey/ConnectionActivity.java
@@ -1,15 +1,9 @@
 package com.baconbanana.easysurvey;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import com.baconbanana.easysurvey.functionalCore.coms.ConnectToServer;
-import com.baconbanana.easysurvey.functionalCore.coms.Get;
-import com.baconbanana.easysurvey.functionalCore.coms.Send;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -22,22 +16,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
-import com.baconbanana.easysurvey.functionalCore.Storage;
+import com.baconbanana.easysurvey.functionalCore.coms.ConnectToServer;
+import com.baconbanana.easysurvey.functionalCore.coms.Get;
+import com.baconbanana.easysurvey.functionalCore.coms.Send;
 
 /**
- * This activity class if a launcher activity that lets the user to connect to 
- * computer by entering its IP and pressing button connect. If there are answers 
- * stored in device it lets you send them, if there is input form computer it lets 
- * you to receive it and sent it using buttons send get. After a survey is saved 
- * you can start the survey.
- * @author Beka, Team BaconBanana 
- *
+ * This activity class if a launcher activity that lets the user to connect to
+ * computer by entering its IP and pressing button connect. If there are answers
+ * stored in device it lets you send them, if there is input form computer it
+ * lets you to receive it and sent it using buttons send get. After a survey is
+ * saved you can start the survey.
+ * 
+ * @author Beka, Team BaconBanana
+ * 
  */
-public class ConnectionActivity extends Activity {
+public class ConnectionActivity extends Activity
+{
 
 	public ConnectionActivity context = this;
 
@@ -53,10 +51,10 @@ public class ConnectionActivity extends Activity {
 	private EditText input;
 	private static boolean isSurveyCompleted = false;
 	private volatile static boolean notFirstTime = false;
-	private static Button send;
-	private static Button get;
-	private static Button start;
-	private static Button connect;
+	private static ImageButton send;
+	private static ImageButton get;
+	private static ImageButton start;
+	private static ImageButton connect;
 	private final String IPADDRESS_PATTERN = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
 			+ "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
 			+ "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
@@ -69,46 +67,53 @@ public class ConnectionActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_getsend);
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-		
+
 		input = (EditText) findViewById(R.id.ip);
 		cb = (CheckBox) findViewById(R.id.remember);
-		send = (Button) findViewById(R.id.send);
-		get = (Button) findViewById(R.id.get);
-		start = (Button) findViewById(R.id.start);
-		connect = (Button) findViewById(R.id.connect);
-	    send.setEnabled(false);
-	    get.setEnabled(false);
+		send = (ImageButton) findViewById(R.id.send);
+		get = (ImageButton) findViewById(R.id.get);
+		start = (ImageButton) findViewById(R.id.start);
+		connect = (ImageButton) findViewById(R.id.connect);
+		send.setEnabled(false);
+		get.setEnabled(false);
 		start.setEnabled(false);
 		IPPreferences = getSharedPreferences("IPPref", MODE_PRIVATE);
 		IPPrefsEditor = IPPreferences.edit();
 		pattern = Pattern.compile(IPADDRESS_PATTERN);
 		saveIP = IPPreferences.getBoolean("saveIP", false);
-		
-		if (saveIP == true) {// <----------set remembered IP
+
+		if (saveIP == true)
+		{// <----------set remembered IP
 			input.setText(IPPreferences.getString("IP", ""));
 			cb.setChecked(true);
 		}
 	}
 
-
-	
-	public void connect(View v) {
+	public void connect(View v)
+	{
 		Log.d("c", "connect called");
-		if (isOnline()) {
-			if (notFirstTime) {
+		if (isOnline())
+		{
+			if (notFirstTime)
+			{
 				new ConnectToServer(inputS, skt, context, IP).execute("");
-				
-			} else {
+
+			}
+			else
+			{
 
 				IP = input.getText().toString();
 
-				if (cb.isChecked()) {
+				if (cb.isChecked())
+				{
 
 					IPPrefsEditor.putBoolean("saveIP", true);
 					IPPrefsEditor.putString("IP", IP);
 					IPPrefsEditor.commit();
 
-				} else {
+				}
+				else
+				{
 
 					IPPrefsEditor.clear();
 					IPPrefsEditor.commit();
@@ -119,19 +124,24 @@ public class ConnectionActivity extends Activity {
 
 				matcher = pattern.matcher(IP);
 				boolean b = matcher.matches();
-				if (b == false) {
+				if (b == false)
+				{
 					AlertDialog.Builder dlgAlert = new AlertDialog.Builder(this);
 
 					dlgAlert.setMessage("Invalid IP");
 					dlgAlert.setPositiveButton("OK", null);
 					dlgAlert.create().show();
-				} else {
+				}
+				else
+				{
 					new ConnectToServer(inputS, skt, context, IP).execute("");
 				}
 
 			}
 
-		} else {
+		}
+		else
+		{
 			AlertDialog.Builder dlgAlert = new AlertDialog.Builder(this);
 
 			dlgAlert.setMessage("No internet connection!");
@@ -141,88 +151,100 @@ public class ConnectionActivity extends Activity {
 
 	}
 
-	public void send(View v) {
+	public void send(View v)
+	{
 		new Send(skt, context).execute("");
 	}
 
-	public void get(View v) {
+	public void get(View v)
+	{
 		new Get(skt, context).execute("");
 	}
 
-	public boolean isOnline() {
+	public boolean isOnline()
+	{
 		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 		NetworkInfo netInfo = cm.getActiveNetworkInfo();
-		if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+		if (netInfo != null && netInfo.isConnectedOrConnecting())
+		{
 			return true;
 		}
 		return false;
 	}
 
-	public void moveToSurveyActivity(View v) throws FileNotFoundException,
-			IOException {
-
+	public void validateUser(View v)
+	{
 		Intent intent = new Intent(this, ValidationActivity.class);
 		startActivity(intent);
-		
 	}
-	public static synchronized void setSurveyCompleted() {
+
+	public static synchronized void setSurveyCompleted()
+	{
 		isSurveyCompleted = true;
 	}
-	public static synchronized boolean isSurveyCompleted() {
+
+	public static synchronized boolean isSurveyCompleted()
+	{
 		return isSurveyCompleted;
 	}
 
-	public static synchronized void setNotFirstTime() {
+	public static synchronized void setNotFirstTime()
+	{
 		notFirstTime = true;
 	}
 
-	public static synchronized void changeGetButton(boolean b) {
+	public static synchronized void changeGetButton(boolean b)
+	{
 		get.setEnabled(b);
 	}
 
-	public static synchronized void  changeSendButton(boolean b) {
+	public static synchronized void changeSendButton(boolean b)
+	{
 		send.setEnabled(b);
 	}
-	public static synchronized void enableStartButton() {
+
+	public static synchronized void enableStartButton()
+	{
 		start.setEnabled(true);
 	}
-	
-	public static synchronized void disableConnectButton() {
+
+	public static synchronized void disableConnectButton()
+	{
 		connect.setEnabled(false);
 	}
-	
-	
-	
-	
-	public static synchronized void setNameAndDate(String s) {
-		nameAndDate=s;
+
+	public static synchronized void setNameAndDate(String s)
+	{
+		nameAndDate = s;
 	}
-	public static synchronized String getNameAndDate() {
+
+	public static synchronized String getNameAndDate()
+	{
 		return nameAndDate;
 	}
-	
-	public static synchronized void setSocket(Socket s) {
-		skt=s;
+
+	public static synchronized void setSocket(Socket s)
+	{
+		skt = s;
 	}
-	public static synchronized Socket getSocket() {
+
+	public static synchronized Socket getSocket()
+	{
 		return skt;
 	}
-	
-	
-	public static synchronized void setInputS(InputStream x) {
-		inputS=x;
+
+	public static synchronized void setInputS(InputStream x)
+	{
+		inputS = x;
 	}
-	
+
 }
 
-	
-	/*@Override
-	protected void onStop(){   	//<----new line
-		super.onStop();	        //<----new line
-		connection.cancel(true);//<----new line
-		}	
-	@Override
-	 protected void onDestroy(){
-		 connection.cancel(true);//<----new line
-	 }*/
+/*
+ * @Override protected void onStop(){ //<----new line super.onStop(); //<----new
+ * line connection.cancel(true);//<----new line }
+ * 
+ * @Override protected void onDestroy(){ connection.cancel(true);//<----new line
+ * }
+ */
 
--- EasySurveyDesigner/src/com/baconbanana/easysurveydesigner/GUI/SendSurveyGetAnswers.java
+++ EasySurveyDesigner/src/com/baconbanana/easysurveydesigner/GUI/SendSurveyGetAnswers.java
@@ -5,17 +5,12 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import java.util.Set;
-
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -55,7 +50,6 @@ public class SendSurveyGetAnswers implements ActionListener {
 	protected JFrame frame, frameIp;
 	
 	Thread connection = new Connection();
-	Thread setIP = new setIP();
 	Thread IPwindow = new Thread();
 	
 	
@@ -73,6 +67,22 @@ public class SendSurveyGetAnswers implements ActionListener {
 				setEverything();
 			    connection.start();
 				connection.join();
+				
+				Thread setIP=new Thread(new Runnable()
+				{
+					
+					@Override
+					public void run() {
+						if (serverSocket == null) {
+							status.setText("Could not connect");
+							changeAccept(false);
+
+						} else {
+							status.setText("Waiting for a device to connect. " + localIP);
+
+						}
+					}
+				});
 				setIP.start();
 				setIP.join();
 				
@@ -114,20 +124,6 @@ public class SendSurveyGetAnswers implements ActionListener {
 				frame.setDefaultCloseOperation(0);
 				
 	}
-
-			public class setIP extends Thread {
-
-				public void run() {
-					if (serverSocket == null) {
-						status.setText("Could not connect");
-						changeAccept(false);
-
-					} else {
-						status.setText("Waiting for a device to connect. " + localIP);
-
-					}
-				}
-			}
 
 			private void TempsendPage() {
 
